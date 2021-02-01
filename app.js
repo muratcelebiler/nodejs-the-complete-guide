2db97fd038c17df19d70ad7d2b7e10d773fdef4c
Murat Çelebiler <muratcelebiler@teknasyon.com>
2021-02-02 00:18:34 +0300

    [feat] body parser added - npm install --save body-parser - put, post, get methods analyze

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // Express kütüphanesini dahil ediyoruz.
 const express = require('express');
 
+// Body parser eklentisini dahil ediyoruz. Bu eklenti ile gelen requesti parse ediyoruz
+const bodyParser = require('body-parser');
+
 // Express core kısmında bir fonksiyon döndüğü için core kısmını initilaize ediyoruz.
 const app = express();
 
+// Body parser ile ana yapımızı kuruyoruz.
+app.use(bodyParser.urlencoded({extended: false}));
+
+// add-product endpointine gidince bir form oluşturduk.
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
 // Middleware ekliyoruz.
 // Middleware sadece belli url adreslerine göre çalışabilirler. Örneğin aşağıdaki middleware sadece product url adresine istek atılınca çalışmaktadır.
-app.use('/product', (req, res, next) => {
-    console.log("Middleware 1");
+// app.use() tüm html methodlarını kapsarken biz aynı zaman app.get(), app.post() etc. ile bir takım methodları özel olarak tanımlayabiliriz. Örneğin product endpointi sadece post isteklerinde çalışmaktadır.
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
 
     // Bu komutu eklemediğimizde bir sonraki aşamaya geçemiyoruz ve bu middleware içerisinde hapsoluyoruz.
     next();
