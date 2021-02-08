9457062cd4dc236a33c9bae7ea5a9f978f101203
Murat Çelebiler <muratcelebiler@teknasyon.com>
2021-02-08 09:07:38 +0300

    [feat] storing data in files via the model

--- models/product.js
+++ models/product.js
@@ -1,5 +1,8 @@
-// Ana product sabitimiz
-const products = [];
+// filesystem core modülünü dahil ediyoruz
+const fs = require('fs');
+
+// path modülünü dail diyoruz
+const path = require('path');
 
 // Product sınıfını oluşturarak export ediyoruz.
 module.exports = class Product {
@@ -9,11 +12,41 @@ module.exports = class Product {
 
     // Save product
     save() {
-        products.push(this);
+        // Verilerin ekleneceği dosya
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+        // Dosyayı okuyoruz
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+
+            if(!err) {
+                // Öncelikle dosyadaki verileri alıyoruz
+                products = JSON.parse(fileContent);
+            }
+
+            // Form'dan gelen veriyi products dizisine ekliyoruz
+            products.push(this);
+
+            // Dosyaya son verileri yazıdıryoruz
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                if (err) {
+                    console.log('Hata: ', err);
+                }
+            });
+        });
     }
 
     // Get all products
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        // Verilerin ekleneceği dosya
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+        fs.readFile(p, (err, fileContent) => {
+            if(err) {
+                cb(err);
+            }
+
+            return cb(JSON.parse(fileContent));
+        });
     }
 };
