59d04d08dfeca5c4eb8b2f1191fbf3de01fed422
Murat Çelebiler <muratcelebiler@teknasyon.com>
2021-02-08 09:40:02 +0300

    [refactor] code refactored for product save by file

--- models/product.js
+++ models/product.js
@@ -4,27 +4,35 @@ const fs = require('fs');
 // path modülünü dail diyoruz
 const path = require('path');
 
+// Verilerin ekleneceği dosya
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+// Ürünleri dosyadan okur
+const getProductsFromFile = (cb) => {
+    // Dosyayı okuyoruz
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            cb([]);
+        } else {
+            // products.json dosyasını silince undefined hatası veriyor. Dosyayı bulamadığı için içinden veri de okuyamıyoruz.
+            fileContent === 'undefined' ? [] : fileContent;
+
+            // Dosyadan dönen dataları okuyoruz
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
 // Product sınıfını oluşturarak export ediyoruz.
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
-    // Save product
+    // Ürün kaydetme
     save() {
-        // Verilerin ekleneceği dosya
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-
-        // Dosyayı okuyoruz
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-
-            if(!err) {
-                // Öncelikle dosyadaki verileri alıyoruz
-                products = JSON.parse(fileContent);
-            }
-
-            // Form'dan gelen veriyi products dizisine ekliyoruz
+        getProductsFromFile(products => {
+            // Form'dan gelen verileri aktarıyoruz
             products.push(this);
 
             // Dosyaya son verileri yazıdıryoruz
@@ -38,15 +46,6 @@ module.exports = class Product {
 
     // Get all products
     static fetchAll(cb) {
-        // Verilerin ekleneceği dosya
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-
-        fs.readFile(p, (err, fileContent) => {
-            if(err) {
-                cb(err);
-            }
-
-            return cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 };
