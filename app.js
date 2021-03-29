e6907c3e41b93bc5422ab51f06e71f2132b37bb1
Murat Çelebiler <muratcelebiler@teknasyon.com>
2021-03-29 23:08:27 +0300

    [refactor] app.js refactored - docker removed in project

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const path = require('path');
-
+// Express
 const express = require('express');
-const bodyParser = require('body-parser');
-
-const errorController = require('./controllers/error');
-
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// Modules
+const path = require('path');
+const bodyParser = require('body-parser');
 
+// Routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+// Controllers
+const errorController = require('./controllers/error');
+
+// Set view config
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+// Error control
 app.use(errorController.get404);
 
 app.listen(3000);
